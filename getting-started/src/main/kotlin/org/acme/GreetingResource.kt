3e6fece947069ccc9f7d4445a24d85770c7a305e
akaitigo <ryusei1116nakayama@outlook.com>
2025-01-08 15:31:36 +0900

    【初めてのアプリケーション作成】6. 依存性注入の使用　完了

--- getting-started/src/main/kotlin/org/acme/GreetingResource.kt
+++ getting-started/src/main/kotlin/org/acme/GreetingResource.kt
@@ -1,12 +1,23 @@
 package org.acme
 
+import jakarta.inject.Inject
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 
 @Path("/hello")
-class GreetingResource {
+class GreetingResource(service: GreetingService) {
+
+    @Inject
+    val service:GreetingService = service
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    fun greeting(name:String):String{
+        return service.greeting(name)
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
