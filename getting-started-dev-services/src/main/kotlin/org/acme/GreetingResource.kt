21e7672f56e4ab6ec4f823668572e9788ba696cf
akaitigo <ryusei1116nakayama@outlook.com>
2025-01-09 11:16:14 +0900

    【2つ目のアプリケーション】3.2データの書き込み 完了

--- getting-started-dev-services/src/main/kotlin/org/acme/GreetingResource.kt
+++ getting-started-dev-services/src/main/kotlin/org/acme/GreetingResource.kt
@@ -1,21 +1,23 @@
 package org.acme
 
+import jakarta.transaction.Transactional
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
+import jakarta.ws.rs.QueryParam
 import jakarta.ws.rs.core.MediaType
 import org.jboss.resteasy.reactive.RestQuery
 @Path("/hello")
 class GreetingResource {
 
     @GET
+    @Transactional
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello(@RestQuery name: String?): String {
-        return if (name.isNullOrBlank()) {
-            "Hello"
-        } else {
-            "Hello $name"
-        }
+    fun hello(@QueryParam("name") name: String?): String {
+        var greeting = Greeting()
+        greeting.name = name.toString()
+        greeting.persist()
+        return "Hello $name"
     }
 
     
