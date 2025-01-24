e519457ed4b76c935ce445d2cff36578ea07bd93
akaitigo <ryusei1116nakayama@outlook.com>
2025-01-24 12:11:05 +0900

    【GCSとの連携】GCS拡張機能の追加

--- getting-started-gcs/build.gradle.kts
+++ getting-started-gcs/build.gradle.kts
@@ -14,6 +14,8 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkiverse.googlecloudservices:quarkus-google-cloud-storage")
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-google-cloud-services-bom:${quarkusPlatformVersion}"))
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-rest")
     implementation("io.quarkus:quarkus-kotlin")
